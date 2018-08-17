16d8a9a285b0d8fbc6597c98f2ec440bb2bdf053
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-08-18 09:20:56 +1200

    Add sub methods to producer

---
+++ src/producer.rs
@@ -0,0 +1,15 @@
+use topic::Topic;
+use message::Message;
+
+pub struct Producer {
+}
+
+impl Producer {
+    pub fn new(_topic: Topic) -> Self {
+        unimplemented!()
+    }
+
+    pub fn publish(_message: Message) {
+        unimplemented!()
+    }
+}
