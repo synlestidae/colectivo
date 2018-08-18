d52f5335ee6550bde5fab1353d71ab59f49b9c6a
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-08-18 16:17:11 +1200

    Change message code a bit

--- src/message.rs
+++ src/message.rs
@@ -4,7 +4,6 @@ use payload::Payload;
 
 #[derive(Clone)]
 pub struct Message {
-    pub index: u64,
     pub key: MessageKey,
     pub payload: Payload
 }
@@ -18,7 +17,6 @@ unsafe impl Send for Message {
 impl Message {
     pub fn new<K: Into<MessageKey>, M: Into<Payload>>(key: K, payload: M) -> Self {
         Message {
-            index: 0,
             key: key.into(),
             payload: payload.into()
         }
--- src/message_key.rs
+++ src/message_key.rs
@@ -3,6 +3,12 @@ use std::convert::From;
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone)]
 pub struct MessageKey(pub Vec<u8>);
 
+impl MessageKey {
+    pub fn from_str(key_str: &str) -> Self {
+        MessageKey(key_str.to_string().into_bytes())
+    }
+}
+
 impl From<String> for MessageKey {
     fn from(string: String) -> Self {
         MessageKey(string.into_bytes())
@@ -13,11 +19,4 @@ impl<'a> From<&'a str> for MessageKey {
     fn from(string: &str) -> Self {
         MessageKey(string.to_owned().into_bytes())
     }
-}
-
-impl MessageKey {
-    pub fn from_str(key_str: &str) -> Self {
-        MessageKey(key_str.to_string().into_bytes())
-    }
-
 }
